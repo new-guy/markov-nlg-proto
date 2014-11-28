1d46eaa81bd235f506187fcdf9b1859c0a6353cc
David Kirk <flagbearer223@gmail.com>
2014-11-28 12:55:58 +0000

    Added new phrases to the datamine and did a bunch of bugfixes

--- js/markov.js
+++ js/markov.js
@@ -1,5 +1,4 @@
-//var human_phrase_dataset =  ["Deny her.", "What if Invokers EMP was based on range from the center?", "Creator of Rick and Morty, Justin Roiland said hes interested in an announcer pack.", "Can we get some fixes for path finding?", "He did it! ", "Fissure is still 1300 length. despite 6.79 buff", "my first TI shirt design, C&C please so i can rearrange elements if you guys think can be improve more :", "SteamDB We recommend NOT using any Steam services until Valve issues a fix", "As requested, an interview with Mason on his time with Evil Geniuses: Artys got awful taste in women, Zais opinion on anime sucks, and PPD refuses to draft me anything but Mirana.", "Hewlett Packard teams up with Valve and ships systems with Dota 2 preinstalled in the future.", "Sing is streaming with puppey, kuro, and bruno.", "600,000,000th match of DOTA 2", "Nerf NOW!! - WHEN YOU GET OLDER", "Matches on Tuesday the 8th of April", "Name a hero you want to get better at and someone who plays that hero can help you do that.", "Early-game tank items: a comparison", "So i was looking for a nice Luna hair and suddenly...", "Suggestion Valve, please let us combine all items of same set as bundle for accessibility and free up space in inventory", "Manta Style used to cancel targetting in DotA 1. Make it happen again, please.", "Introducing HighGroundTV - A New Home-Grown Studio casting ADL Season 2!", "In Defense of Envy", "LoL veteran wanting to step to your side needs a coach", "Suggestion Let us alt-click on the clock on the top of the screen to note the current time", "New Dota 2 team - Team Immunity Shatan, Godot and r1sk", "The Story of DOTA", "I found the legendary 1v1 mid EE vs RTZ practice VOD!", "3xlinkens + shallow grave to save Loda  - 1:01", "Bootseeker", "Will TI4 have the inside team-chat as MLG Columbus did? Volvo pls!", "I made the Invoker cheatsheet which was used as a submission for the TI4 workshop along with 2 other illustrations Ive made. Have a look!", "Offering up some free housing for the duration of Ti4", "NaVi vs Cloud9", "Fails of the Week - Ep. 95  - 6:01", "Item Discussion of the Day: Rod of Atos", "Sorry but I wont let my reputation be destroyed by mere keys - Julz", "Lysander is coaching me and my friend who is new to Dota from LoL on Twitch!", "I translated what someone said to me in Spanish to English and this is what he said...", "Qualifier for Fragbite Masters today at 18:00 CEST!!! Dont forget to sign up!!!", "Solar Crucible, Simple Phoenix design my gf and I made", "Ill Miss Those Noisy Birds In My Ear - Treant Protector Fanart by me", "Some Polymer Clay models that my friend made", "Art Prints and Tees, help upvote to make them a reality!", "Does this have anything with DOTA 2 also?", "This is a MAJOR problem, Volvo. MAJOR.", "Why dont the shopkeepers react to a defeat?", "Bots using cosmetics", "In 3 hours the qualifier for Fragbite Masters 2014 begins!", "Does Tusk need some buffs?", "In-game translation tool I found actually works", "Suggestion: Silence/Doom/Hex/etc should only put bars over the icons of skills they disable.", "Clock Masters Helmet item for Clockwerk on the Steam Workshop", "New Drawings that I didnt get to share... by me", "Please help a new player out.", "Bruno having fun with his new toy Fixed", "Come and check out my Tusk T-shirt that I designed for the International Workshop.", "Looking for a bro to split a hotel with @ TI4 no homo", "Dota 2 Manhua. Chapter 1 translated by a kind soul. Someone who understands Chinese help him/her?", "Shagbark Sweded!  - 0:44", "Do you like stats? My Leaderboard statistics project", "Fnatic.N0tailMeepo does his thing in a pub game  - 6:45", "1v1 and 5v5 amateur tournaments? FIVE DAYS A WEEK? $1000 in prizes?!?!?!?", "Stanley Parable announcer isnt dead!", "mYinsanity picks up DotA 2 Team", "TI4 Workshop shirt :: Darude sandstorm!", "Team Liquid Dota 2 Power Rank - April 2014", "Kunkkas Initial Batch of Submissions for DotA 2 Merchandise Shop", "Is International 4 Free To Watch Or Am I Supposed To Buy It?", "Some questions regarding controls", "Question about Medusa damage calculation", "Why wards in pairs?", "It should be communicated to everyone that they can watch TI4 in-game for FREE!", "Some DotA 2 Community Questions.", "DotaCinema, please upload the models for your April fools joke since it wont be added to the game", "Starladder tie breaker", "American Dota League Season 2 Update", "I just gained MMR from an abandon", "What is a change you DONT want to see in 6.81?", "Match 600497589. How did they throw this?", "Help! Dota 2 question", "ESL One Frankfurt page is live - including a forum for travel, car sharing etc.", "My birthday cake with a hint of Dota Desc in comment", "Does anyone else find themselves being insanely mean in this game? Ive basically been a horrible person. Serious discussion", "Mmm.. an unexpected warning ?", "In 2 hours ASUS ROG DreamLeague Season 1 Day 16 begins with Na’Vi vs Team Empire, Sigma vs Team Dog, Sigma vs Alliance - $200,000+ in Prize Money", "Q Is it possible to remove the unusual effect from the TI3 Smeevil and place it into another courier?", "Improve the mute system", "Teamwork vs. Individual Skill", "Great Comeback 4v5 ruined in the last 5 mins", "How to force dota 2 to find match on a specific server IP range?", "WR-Drow shirt design: A Cold Wind Blows~ What can be done to make it look more wearable :D?", "Commonly seeing this Wraith King item build...", "Given that both heroes are at level 25 with all skills/attributes learned, and no items are allowed, who do you think would win a straight up 1vs1 fight with any hero?", "Join Dota: How To Find Team Page!", "Accurate reviews are accurate", "Chen, Your opinions", "When crafting an item with a scroll add crafted by to its description", "APD - Ranked ap replacment", "Quick Qn regarding Spectre Desolate...", "If you could give any skill global range, what would be the most OP?", "Does size matter", "Epic games from the past two weeks?", "ESL One Frankfurt European Open Bracket Finished", "I made a real ward with a moving eye - UPDATE", "Benefits of key-binding the courier and not just clicking the courier button?", "I want your opinion - Dota 2 Hardcore Mode if it was a thing", "Ember Spirit: Maxing Searing ChainsQ vs. maxing Flame GuardE", "I can agree people use bots to boost MMR, but fucking hell -- Ability Draft?", "NEW HONEST INTRO honest DOTA 2 trailer dopatwo  - 0:55", "Does Icarus Dive sometimes not work?", "More promises from Bulba", "Whats an easy thing in Dota 2 that you CANT do?", "Is there a way to get true fog of war when spectating? Heroes are always shown on minimap no matter what I select unless they smoke.", "Wallpaper my friend made after those timbersaw faceswap pictures that were here a while ago.", "Linux users, hows your Dota 2 performance?", "which heroes in your opinion are high skill based that dont require micro managing other units ?", "DOTA2 Builds - loading server side issue again?", "Super clowny teamfight in a game between Birdgang? and MVP Hot6  - 2:18", "The International Posters by Konras&Madzia", "Godz on Titan: Net takes playing initiator to the extreme!", "Why is it hard to be a noob?", "I made a Dota 2 t-shirt design: Shifting Tides", "HyperX D2L Western Challenge Cloud 9 Double Header in 1 HOUR vs EG and NaVi", "What MMR rating is equivalent to bronze and silver etc... in League or SC?", "Go Dota  - 2:14", "SPOILER Congrats to the teams qualified for the Starladder finals from Europe!", "BountyHunter Series: Virtus.Pro vs. Team DoG", "Looking for roommate for TI4", "Friend did a simple t-shirt entry for the international. I think its pretty cool.", "ET set posted last week. 1/2 on workshop still a work in progress.", "Dota 2: The Stealth Mode Run", "Korean Teams In TI4", "mYinsanity.eu picks up former team One aka Sensei", "Guide for counterpicking", "Suggestion If a player never spawns at the beginning of the game level 0, dont let the game count.", "Friends and I streaming from the trench! We hate each other! Come join!", "South Park megakills announcer", "Viability of double jungle?", "Switching the dont forgot to report/commend around at the start of games", "Boston-Area College Players! :D", "X-POST from /r/SteamPSA SteamDB recommends you to avoid using Steam services because of a vulnerability", "Tidehunter polo shirt 80% off at Thinkgeek.com.", "Bots not only used for MMR boosting. Recentley found bots in ability draft", "Master Assassin Set", "Region locked queue?", "never been so dissapointed at a witch doctor rampage  - 0:33", "What do you do when you get losing streaks?", "Dayumm Aysee with the counter", "Random Featured Hero", "Episode 12 of X Marks the News  - 4:47", "Using Black Map", "Spirit Breaker gets cock blocked.", "TI4 t-shirts Art Guidelines", "Found a MASSIVE bug,but cant post it on dev forums cause people will start abusing it too hard,HOW DO I CONCACT VALVE to report it as a secret information?", "Double tap control group broken", "What is the correct order of a full Voljin combo?", "MMR farming on a whole new level", "How will they make cosmetics for IO?", "Any tips about how to get out of 2k trench tier?", "TI4 Chibi T-shirt - Currently 5 heroes in collection", "Create a Catchy Slogan. Win EZ Arcana.", "Do you all think this year will be a host to a 2 Million Dollar price pool for first place?", "The Dream, it is alive - EoT.Hammer vs. MVP.hot6 @ The Inaugural  - 1:16", "9/10 of all traders that add me", "Im likely going to TI alone - anyone looking for a +1 on their trip? from Toronto", "This pretty much sums up DOTA 2 for me a lot of the time dopatwo", "Would any 5k+ mid players mind playing a couple 1v1 mids against me? I wanna learn some stuff.", "Seperate control groups per illusion.", "quick question regarding RD, dont upboat", "Hour long interview with the one and only Ayesee. Im the guy that knows nothing about Dota. My friend is the guy that knows plenty about Dota but was also very nervous. Enjoy.", "Dota Battles 3k-4k MMR League starts in 1 hour!", "Streaming for Cancer", "For those interested in leaderboards, dotamax.com has an interesting feature.", "All Random, Vol. 6! Youre not bored of these, right?  - 3:00", "Favorite item", "I dont know who made this, but Id like to see this at TI4", "Is battlefury ever a viable pickup on sven?", "A Compilation of Headshots  - 0:59", "New model for mega creep!", "Pudge plays DOTA 2 abridged  - 1:25", "Any fellow Aussies for some unranked fun?", "With the large amount of bugs still present in the game, who would prefer getting a large bug fix patch next rather than a content patch?", "the shit I see while browsing /d2g/", "JoinDota down?", "Ti4 Hoodie DesignTidehunter and Warlock", "Spectre VODs from players?", "Is Dota 2 Test still relevant?", "Is there any new combos if the unreleased Ability Draft Heroes are released?", "Dota 2 has stopped working pops up as the queue pops.", "What mechanical changes can be added to various heroes to make the game better?", "Faceless Ursa Chronosphere", "Question about Early, Average, Late timings", "My first try to make Custom Animation", "Does Ranked MM affect your non ranked mms mmr?", "xXCloCkTic NaTi0nXx  - 0:45", "Solution to volvo plz-type disconnects where you cant reconnect", "None of these 5 wards have seen each other. What a game!!!", "Just submitted something for the Faceless fans in the workshop.", "Sometimes owning a DC-hook lets you meet the most bizarre people", "Dotagon.com", "Why is Ember Spirit so widely banned now?"];
-var human_phrase_dataset = ["meet me at market square", "let me know if that will work for you", "we don't want the game to last too long", "we'll have some drinks", "I'm back in Durham tomorrow", "I will come up and visit", "I have a sick blow up bed", "we can sit together", "no girls around", "it will start slowly", "no girls allowed", "I hope people remember this", "I sure as shit will remember this", "this has nothing to do with politics", "It's pretty fantastic", "It does give you powerful tools", "the tutorial doesn't seem that bad", "explain why all of these things are on the screen", "visit when it is time to visit", "this phrase was made by a human", "this phrase was not made by a human", "hopefully people will read this", "please help I'm trapped in the computer", "this one was written by the algorithm", "I am staying at Subodha's tonight", "it is dangerous to wear blue", "it can be scary outside at night", "it is starting to get a bit dark", "the walls around me are white", "I have a chicago bears hat to my right", "hopefully this will be enough", "I am not sure how many phrases will be needed", "We can try using thirty five", "Although I am not sure how much these will line up", "I need an algorithm for rendering the chain visually"];
+var human_phrase_dataset = ["meet me at market square", "let me know if that will work for you", "we don't want the game to last too long", "we'll have some drinks", "I'm back in Durham tomorrow", "I will come up and visit", "I have a sick blow up bed", "we can sit together", "no girls around", "it will start slowly", "no girls allowed", "I hope people remember this", "this has nothing to do with politics", "It's pretty fantastic", "It does give you powerful tools", "the tutorial doesn't seem that bad", "explain why all of these things are on the screen", "visit when it is time to visit", "this phrase was made by a human", "this phrase was not made by a human", "hopefully people will read this", "please help I'm trapped in the computer", "this one was written by the algorithm", "I am staying at Subodha's tonight", "it is dangerous to wear blue", "it can be scary outside at night", "it is starting to get a bit dark", "the walls around me are white", "I have a chicago bears hat to my right", "hopefully this will be enough", "I am not sure how many phrases will be needed", "We can try using thirty five", "Although I am not sure how much these will line up", "I need an algorithm for rendering the chain visually", "I'm so pumped right now", "Why would you let your dog watch that", "you get called Joe sometimes", "this sentence is a lie", "there are fifty five snails on the wall", "not to be sold separately", "that shirt looks great on you", "give me the laptop", "would you like any chocolate", "what are you gonna do", "that was probably the right choice", "How's it going?", "I tried to think of something clever, I failed.", "I'm just going to the library.", "Hey! How's it going?", "Your hair looks nice.", "Russia will not stand for this", "How do you do?", "Will do.", "How is it going?", "What have you been up to?", "Sounds great.", "What are you doing tomorrow? ", "Did you watch the game? ", "How's it going?", "How about those Chicago Bears?", "Right, see you later.", "What's good?", "You wanna go?", "What's up dog?", "What, seriously?", "How's it going?", "Cold day, isn't it?", "Do you know what time it is?", "It's so great to see you!", "How are you feeling?", "Where should we meet?", "Hey man, how was the show last night?", "I'm so hungry!  Are you?", "What are you up to today?", "How's your mom doing?", "Where do you work?", "How's it going? ", "I'm happy to help. ", "How you doin'?", "You lookin' fine, homeboy!", "Hold your horses!", "Gorillas like bananas", "Hey, what's up?", "Do you need help?", "Good luck!", "how was your day", "Good day, sir.", "Hey how are you?", "Let's go to the Thai supermarket to eat lunch"];
 //var human_phrase_dataset = ["I am hungry", "I am full", "I am David"];
 /* VIEW CONTROLLER */
 
@@ -17,7 +16,7 @@ function initPage()
 function setupNewRound()
 {
 	var human_phrase = getSinglePhrase();
-	var algorithm_phrase = getAlgorithmPhrase(2, static_markov_chain);
+	var algorithm_phrase = getAlgorithmPhrase(3, static_markov_chain);
 
 	if(Math.random() > 0.5)
 	{
@@ -76,6 +75,13 @@ function evalTie()
 	}
 
 	if(AI_SHOULD_LEARN) addPhraseToMarkovChain(ai_phrase, static_markov_chain);
+
+	var tie_count = parseInt($("#tiecounter").attr("count")) + 1;
+
+	$("#tiecounter").attr("count", tie_count);
+	$("#tiecounter").text("Ties: "+ tie_count);
+
+	$("#previousguess").text("Tie!");
 
 	setupNewRound();
 }
@@ -206,6 +212,8 @@ function addPhraseToMarkovChain(phrase, chain)
 
 function addWordToWeightArray(word, weight_array)
 {
+	if(typeof word == "undefined") return weight_array;
+
 	for(var i = 0; i < weight_array.length; i++)
 	{
 		var working_weight = weight_array[i];
@@ -228,30 +236,47 @@ function chainIsNotValid(chain)
 
 /* PHRASE GENERATOR */
 
-var choice = function(array) 
+var weightedChoice = function(array) 
 {
-	var i = Math.floor(array.length * Math.random());
-	return array[i];
-};
+	var weight_sum = 0;
 
-var max_attempts = 10;
-var attempt_counter = 1;
+	for(var i = 0; i < array.length; i++)
+	{
+		weight_sum += array[i].weight;
+	}
+
+	var weighted_index = Math.floor(Math.random()*weight_sum);
+
+	for(var i = 0; i < array.length; i++)
+	{
+		weighted_index -= array[i].weight;
+
+		if(weighted_index <= 0) return array[i];
+	}
+
+	return array[array.length-1];
+};
 
 var getAlgorithmPhrase = function(min_length, working_chain)
 {
-	var word = choice(working_chain.startwords).word;
+	var word = weightedChoice(working_chain.startwords).word;
 	var phrase = [word];
 
 	while(typeof working_chain.corewords[word] != "undefined")
 	{
 		var next_words = working_chain.corewords[word].next_words;
-		word = choice(next_words).word;
+
+		var next_word_object = weightedChoice(next_words);
+		if(typeof next_word_object == "undefined") break;
+
+		word = next_word_object.word;
 		phrase.push(word);
+
 		if(phrase.length > min_length && working_chain.corewords[word].terminal) break;
 	}
-	if (phrase.length < min_length && attempt_counter < max_attempts)
+
+	if(phrase.length < min_length)
 	{
-		attempt_counter += 1;
 		return getAlgorithmPhrase(min_length, working_chain);
 	}
 
